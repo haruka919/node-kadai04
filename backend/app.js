b6acb2f8c235a5240353de6cc293aee460ba0a4d
parukoo <hrk.0u0x@gmail.com>
2020-10-31 20:34:28 +0900

    ルーティング設定

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 
- app.get('/', (req, res) => {
-    return res.send('Hello world');
-});
+const indexRouter = require('./routes/index');
+const loginRouter = require('./routes/login');
+const registerRouter = require('./routes/register');
+
+// テンプレートエンジンの指定
+app.set('view engine', 'ejs');
+
+// routeの設定
+app.use('/', indexRouter);
+app.use('/login', loginRouter);
+app.use('/register', registerRouter);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
