9e50c311be65b39f335a62e1ce19a64ebec5b5fb
parukoo <hrk.0u0x@gmail.com>
2020-11-03 14:19:26 +0900

    JWTの設定ファイルを追加

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  jwt: {
+    secret: '3b9326859321fb46cb20ed7c8587426a8db7e666a0cab5a916d8fd78c68b539f8b0b3b48626b953d549d29e0b3147a1aae803f741262e0a26cb2438c10bc579f',
+    options: {
+      algorithm: 'HS256',
+      expiresIn: '10m'
+    }
+  }
+};
