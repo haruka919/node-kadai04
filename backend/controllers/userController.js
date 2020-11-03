36fcdbd924004033d50025a183aef86948df6b80
parukoo <hrk.0u0x@gmail.com>
2020-11-03 15:00:57 +0900

    新規登録のバリデーションを追加

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,7 @@
-const mysql = require('mysql')
+const mysql = require('mysql');
 const jwt = require('jsonwebtoken');
 const config = require('./../config.js');
-const { cookie } = require('express-validator');
+const { validationResult } = require('express-validator/check');
 
 const knex = require('knex')({
   client: 'mysql',       // 使用するデータベースを指定
@@ -31,14 +31,32 @@ module.exports = {
       form: {
         name: '',
         email: '',
-      }
+      },
+      content: '',
     }
-    res.render('./register.ejs', data)
+    res.render('./register.ejs', data);
   },
 
 
   // ユーザー登録の送信処理
   store (req, res) {
+    // バリデーションの結果にエラーがあるかのチェック
+    const errors = validationResult(req);
+    // バリデーション失敗
+    if (!errors.isEmpty()) {
+      let content = '<ul class="error">';
+      const result_arr = errors.array();　　// エラー内容が配列に返る
+      for (let n in result_arr) {
+        content += '<li>' + result_arr[n].msg + '</li>';
+      }
+      content += '</ul>';
+      const data = {
+        title: '会員登録',
+        content: content,
+        form: req.body,　// nameとemailは入力したものを返す
+      };
+      res.render('register', data);
+    } else {
     new User({
       name: req.body.name,
       email: req.body.email,
@@ -65,7 +83,11 @@ module.exports = {
       });
 
       res.redirect('/');
-    });
+    })
+    .catch(() => {
+      res.redirect('/register');
+    })
+  }
   },
 
   // ログイン処理
--- backend/validators/RegistValidator.js
+++ backend/validators/RegistValidator.js
@@ -5,7 +5,7 @@ module.exports = [
   check('email').isEmail().withMessage('Email addressはメールアドレスを入力して下さい。'),
   check('password').isLength({ min: 7 }).withMessage('Passwordは7文字以上で入力して下さい。'),
   check('password').custom( (value, { req }) => {
-    if (req.body.password !== req.body.comfirm_password) {
+    if (req.body.password !== req.body.password_confirm) {
       throw new Error('Password（再入力）と一致しません。');
     }
     return true;
