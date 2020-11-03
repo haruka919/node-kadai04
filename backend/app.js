ed8acabc78199152c3e5ca067130205919f1432c
parukoo <hrk.0u0x@gmail.com>
2020-11-03 14:31:09 +0900

    クッキーとJWTを使用するためのプラグインを追加

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
+const cookieParser = require('cookie-parser')
+
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const config = require('./config.js');
 
 const indexRouter = require('./routes/index');
 const loginRouter = require('./routes/login');
@@ -11,9 +15,15 @@ const createRouter = require('./routes/create');
 // テンプレートエンジンの指定
 app.set('view engine', 'ejs');
 
+// リクエストヘッダに設定されたCookieを読み取るための設定
+app.use(cookieParser())
+
 // POSTデータを取得する時に必要
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// staticメソッドを利用し、指定ディレクトリ以下の静的ファイルを読み込む
+app.use(express.static('public'));
 
 // routeの設定
 app.use('/', indexRouter);
