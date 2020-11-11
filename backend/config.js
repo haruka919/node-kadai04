b8b7a1ee9d4af40fbaf300aff39514a9ecb02620
parukoo <hrk.0u0x@gmail.com>
2020-11-11 21:34:04 +0900

    JWTに使用する変数を環境変数に置き換えた

--- backend/config.js
+++ backend/config.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+const env = process.env
+
 module.exports = {
   jwt: {
-    secret: '3b9326859321fb46cb20ed7c8587426a8db7e666a0cab5a916d8fd78c68b539f8b0b3b48626b953d549d29e0b3147a1aae803f741262e0a26cb2438c10bc579f',
+    secret: env.JWT_SECRET,
     options: {
-      algorithm: 'HS256',
-      expiresIn: '10m'
+      expiresIn: env.JWT_EXPIRES_IN
     }
   }
 };
